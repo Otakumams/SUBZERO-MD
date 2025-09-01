8030f7b2a76fbe5cfa5b1a90cf302f43953ac1b6
Otakumams <153320468+Otakumams@users.noreply.github.com>
2025-09-01 12:16:43 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
   SESSION_ID: "SUBZERO~fe100d6c878e986f14e9ffa2b0ce0478e50bb4de", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  OWNER_NUMBER: "243842932305", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
